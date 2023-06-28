66a9d497a4b383c006027d843aa1d25bcd15c491
e <sharriewang2022@gmail.com>
2023-06-28 11:04:16 +0800

    duck

---
+++ Duck/app/src/main/java/com/hfad/duck/MainActivityDuck.java
@@ -0,0 +1,15 @@
+package com.hfad.duck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivityDuck extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_duck);
+    }
+}
